b9b8e363b970aafd98f5c0eaeff0a0bd5764e29b
mzakyrbn <muhammad.zaky53@ui.ac.id>
2026-09-08 20:40:40 +0700

    tutorial2_test2

--- main/views.py
+++ main/views.py
@@ -4,19 +4,20 @@ from main.models import Experience
 
 def show_main(request):
     context = {
-        "name": "Burhan",
-        "npm": "2206000000",
+        "name": "Muhammad Zaky Robbani",
+        "npm": "2506597712",
         "study_program": "S1 Ilmu Komputer",
         "bio": (
-            "Mahasiswa Ilmu Komputer Universitas Indonesia yang tertarik "
-            "pada pengembangan perangkat lunak dan pendidikan."
+            "CS student at Universitas Indonesia thats only just started coding after done doing the uni test. "
+            "Due to having a gaming hobby on early age, my interest are slowly evolving into wanting to understand "
+            "how game works and that lead me into taking career in computer world which is align with my interest."
         ),
     }
     return render(request, "index.html", context)
 
 def show_experience(request):
     context = {
-        "name": "Burhan",
+        "name": "Muhammad Zaky Robbani",
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
